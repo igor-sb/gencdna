25230a9dd716ed48e124856aa7025a44b9dbd993
igor-sb <>
2023-07-29 21:18:03 -0700

    cleanup linting errors

--- pacbio_qc/phred_scores.py
+++ pacbio_qc/phred_scores.py
@@ -1,7 +1,7 @@
 """Phred score manipulation functions."""
 
-import Bio
 import numpy as np
+from Bio import SeqRecord
 from numpy.typing import NDArray
 
 
@@ -9,6 +9,6 @@ def convert_phred_scores_to_probability(phred_scores: NDArray) -> NDArray:
     return np.power(10, -np.array(phred_scores) / 10)
 
 
-def expected_number_of_errors(fastq_record) -> float:
+def expected_number_of_errors(fastq_record: SeqRecord) -> float:
     phred_scores = np.array(fastq_record.letter_annotations['phred_quality'])
     return np.sum(convert_phred_scores_to_probability(phred_scores))
--- tests/test_phred_scores.py
+++ tests/test_phred_scores.py
@@ -1,9 +1,10 @@
 """Test phred score manipulation functions."""
 
 import gzip
+import pickle
 
-import Bio
 import pytest
+from Bio import SeqIO
 
 from pacbio_qc.phred_scores import expected_number_of_errors
 
@@ -16,9 +17,12 @@ def fixture_example_fastq_file():
 def test_expected_number_of_errors(example_fastq_file, snapshot):
     actual_expected_errors = []
     with gzip.open(example_fastq_file, 'rt') as fq:
-        for fq_record in Bio.SeqIO.parse(fq, 'fastq'):
+        for fq_record in SeqIO.parse(fq, 'fastq'):
             actual_expected_errors.append(
                 expected_number_of_errors(fq_record),
             )
     snapshot.snapshot_dir = 'tests/snapshots'
-    snapshot.assert_match(actual_expected_errors)
+    snapshot.assert_match(
+        pickle.dumps(actual_expected_errors),
+        'example_ccs_expected_errors.pkl',
+    )
