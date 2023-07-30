3dec8398fbc6589ee319150cab18f679a3368c0e
igor-sb <>
2023-07-29 21:44:43 -0700

    add print debug to unit test

--- tests/test_phred_scores.py
+++ tests/test_phred_scores.py
@@ -11,6 +11,9 @@ from pacbio_qc.phred_scores import expected_number_of_errors
 
 def test_expected_number_of_errors(example_fastq_file, snapshot):
     actual_expected_errors = []
+    print('File: {example_fastq_file}'.format(
+        example_fastq_file=example_fastq_file,
+    ))
     with gzip.open(example_fastq_file, 'rt') as fq:
         for fq_record in SeqIO.parse(fq, 'fastq'):
             actual_expected_errors.append(
