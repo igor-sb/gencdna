cc4544cf0cefc64bf03ea20bd50a89b8167afeb0
igor-sb <>
2023-07-30 21:03:26 -0700

    refactor Blast, start on unit tests

--- pacbio_qc/blast.py
+++ pacbio_qc/blast.py
@@ -1,34 +1,40 @@
 """Interface for running command line blastn executable."""
 
-import os
 import subprocess  # noqa: S404
 
 import yaml
+
+
+def is_executable_available(executable_path: str) -> bool:
+    cmd = subprocess.run([executable_path], capture_output=True)
+    return cmd.returncode == 1
 
 
 class Blast(object):
 
     def __init__(
         self,
-        executable_path: str,
+        query: str,
+        subject: str,
         config_yml: str = 'config/blast.yml',
     ) -> None:
         with open(config_yml) as config_file:
             self.config = yaml.safe_load(config_file)
-        if not os.path.exists(executable_path):
-            raise FileNotFoundError(executable_path)
-        self.executable_path = executable_path
+        if not is_executable_available(self.config['executable_path']):
+            raise FileNotFoundError(self.config['executable_path'])
+        self.query = query
+        self.subject = subject
 
-    def create_command(self) -> list[str]:
-        cmd = [self.executable_path]
+    def create_blast_command(self) -> list[str]:
+        cmd = [self.config['executable_path']]
         for arg_flag, arg_value in self.config:
             cmd.extend([
                 '-{arg_flag}'.format(arg_flag=arg_flag),
                 str(arg_value),
             ])
+        cmd.extend(['-query', self.query, '-subject', self.subject])
         return cmd
 
-    def run(self) -> str: # check if its a str or if even we want to return anything
-        cmd = self.create_command()
-        blast_result = subprocess.run(cmd, capture_output=True)  # noqa: S603
-        return blast_result.stdout
+    def run(self) -> subprocess.CompletedProcess:
+        cmd = self.create_blast_command()
+        return subprocess.run(cmd, capture_output=True)  # noqa: S603
