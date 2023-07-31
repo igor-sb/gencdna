6528b561bfc491221d3317d0e2f52f5c88a8b8ee
igor-sb <>
2023-07-31 14:01:19 -0700

    add pandas as blast parser

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,3 +27,17 @@ def repeated_bases_flagged_fastq_file():
     return '{fixture_path}/repeated_bases_flagged.fastq'.format(
         fixture_path=fixture_path,
     )
+
+
+@pytest.fixture()
+def repeated_bases_flagged_fasta_file():
+    return '{fixture_path}/repeated_bases_flagged.fasta'.format(
+        fixture_path=fixture_path,
+    )
+
+
+@pytest.fixture()
+def target_with_repeated_bases_fasta_file():
+    return '{fixture_path}/targets_repeated_bases.fasta'.format(
+        fixture_path=fixture_path,
+    )
--- tests/test_blast.py
+++ tests/test_blast.py
@@ -2,7 +2,10 @@ import pytest
 
 from pacbio_qc.blast import is_executable_available, Blast
 
-@pytest.mark.skipif(not is_executable_available('blastn'), reason='blastn not in path or not installed')
+@pytest.mark.skipif(
+    not is_executable_available('blastn'),
+    reason='blastn not in path or not installed'
+)
 def test_blast_run():
     blast = Blast()
     # test repeated_bases_flagged.fasta (query) vs targets_repeated_bases.fasta (subject)
