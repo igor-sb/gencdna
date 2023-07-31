e4f1d45adcecdbcbecdd0df045c3aeef25fd257d
igor-sb <>
2023-07-31 14:24:45 -0700

    delint and fix unit test for blast

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,17 @@ fixture_path = '{base_path}/tests/fixtures'.format(
     base_path=os.path.abspath('.'),
 )
 
+snapshot_path = '{base_path}/tests/snapshots'.format(
+    base_path=os.path.abspath('.'),
+)
+
+
+@pytest.fixture()
+def snapshot_dir():
+    return '{snapshot_path}'.format(
+        snapshot_path=snapshot_path,
+    )
+
 
 @pytest.fixture()
 def example_fastq_file():
@@ -40,4 +51,11 @@ def repeated_bases_flagged_fasta_file():
 def target_with_repeated_bases_fasta_file():
     return '{fixture_path}/targets_repeated_bases.fasta'.format(
         fixture_path=fixture_path,
+    )
+
+
+@pytest.fixture()
+def blast_output_from_repeated_bases_vs_target():
+    return '{snapshot_path}/blast_out_repeatedbases_vs_target.tsv'.format(
+        snapshot_path=snapshot_path,
     )
--- tests/test_blast.py
+++ tests/test_blast.py
@@ -1,11 +1,31 @@
+"""Test blastn subprocess calls and output parsing."""
+import os
 import pytest
 
-from pacbio_qc.blast import is_executable_available, Blast
+from pacbio_qc.blast import Blast, BlastOutputParser, is_executable_available
+
 
 @pytest.mark.skipif(
     not is_executable_available('blastn'),
-    reason='blastn not in path or not installed'
+    reason='blastn not in path or not installed',
 )
-def test_blast_run():
-    blast = Blast()
-    # test repeated_bases_flagged.fasta (query) vs targets_repeated_bases.fasta (subject)
+def test_blast_run(
+    repeated_bases_flagged_fasta_file,
+    target_with_repeated_bases_fasta_file,
+    blast_output_from_repeated_bases_vs_target,
+    snapshot,
+):
+    blast = Blast(
+        query=repeated_bases_flagged_fasta_file,
+        subject=target_with_repeated_bases_fasta_file,
+    )
+    blast.config['alignment']['word_size'] = 4
+    blast_output = blast.run()
+    blast_output_df = BlastOutputParser(blast_output).output_as_dataframe()
+    snapshot.snapshot_dir = os.path.dirname(
+        blast_output_from_repeated_bases_vs_target,
+    )
+    snapshot.assert_match(
+        blast_output_df.to_csv(None, sep='\t', index=False),
+        blast_output_from_repeated_bases_vs_target,
+    )
