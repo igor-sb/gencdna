9d133961a1072a16696163eae9387e7ce0070d5d
Igor Segota <84479009+igor-sb@users.noreply.github.com>
2023-08-11 19:59:53 -0700

    add fasta read length summary (#11)
    Co-authored-by: igor-sb <>

---
+++ pacbio_qc/fasta_stats.py
@@ -0,0 +1,20 @@
+"""FASTA file statistics."""
+
+import pandas as pd
+from Bio import SeqIO
+
+from pacbio_qc.fastx import open_fastx_or_fastxgz
+
+
+def summarize_fasta_read_lengths(fasta_file: str) -> pd.DataFrame:
+    read_length_counts: dict[int, int] = {}
+    with open_fastx_or_fastxgz(fasta_file, 'rt') as fasta_handle:
+        for fasta_record in SeqIO.parse(fasta_handle, 'fasta'):
+            read_length = len(str(fasta_record.seq))
+            read_length_counts[read_length] = (
+                read_length_counts.get(read_length, 0) + 1
+            )
+    return pd.DataFrame({
+        'read_length': read_length_counts.keys(),
+        'count': read_length_counts.values(),
+    }).sort_values(by=['count'], ascending=False)
