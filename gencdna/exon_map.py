eb652c134c6f9546ecf7d9963254586a4d563390
igor-sb <>
2023-08-13 18:43:34 -0700

    fix linting

--- gencdna/exon_map.py
+++ gencdna/exon_map.py
@@ -1,15 +1,16 @@
 """Create exon maps, e.g. "1 2' 3" for read with exons 1, 2rc and 3."""
 
 import re
+
 import pandas as pd
 
 
 def parse_number_from_exon_id(exon_id: str) -> int:
-    return int(re.sub('^exon_([0-9]+)$', '\\1', exon_id))
+    return int(re.sub('^exon_([0-9]+)$', '\\1', exon_id))  # noqa: WPS342
 
 
 def determine_orientation(row: pd.Series) -> str:
-    if row['subject_start'] <= row['subject_end']:        
+    if row['subject_start'] <= row['subject_end']:
         return '+'
     return '-'
 
@@ -24,7 +25,7 @@ def swap_subject_start_with_stop(blast_out: pd.DataFrame) -> pd.DataFrame:
     rows_with_rc_exons: list[bool] = blast_out['exon_orientation'] == '-'
     blast_out.loc[
         rows_with_rc_exons,
-        ['subject_start', 'subject_end'],
+        ['subject_start', 'subject_end'],  # noqa: WPS359
     ] = blast_out.loc[
         rows_with_rc_exons,
         ['subject_end', 'subject_start'],
@@ -41,12 +42,12 @@ def create_exon_map(blast_out: pd.DataFrame) -> pd.DataFrame:
         axis=1,
     )
     blast_out['exon_label'] = blast_out.apply(create_exon_label, axis=1)
-    blast_out = swap_subject_start_with_stop(blast_out)    
+    blast_out = swap_subject_start_with_stop(blast_out)
     return (
         blast_out
         .sort_values(by=['subject_start'])
         .groupby('subject_id')['exon_label']
-        .apply(lambda x: ' '.join(x), )
+        .apply(lambda exon_label: ' '.join(exon_label))
         .reset_index()
         .rename(columns={'exon_label': 'exon_map'})
     )
